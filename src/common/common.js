d58f685e0a6b7d186dbc48d33f2be174d54e3777
daxing.li <daxing_li@cathaypacific.com>
2020-03-28 20:51:02 +0800

    append something

--- src/common/common.js
+++ src/common/common.js
@@ -2,6 +2,8 @@ function test(type) {
   var root = document.getElementsByClassName("root")[0];
   var p = document.createElement("p");
   var text = document.createTextNode(`${type} world`);
+  var da = document.createComment("who are you ?");
+  text.appendChild(da);
   p.appendChild(text);
   root.appendChild(p);
 }
